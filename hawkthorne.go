af8f130a82c4aea46e682ce2701abce760829f9a
Kyle Conroy <kyle.j.conroy@gmail.com>
2012-12-24 01:44:45 -0800

    Port old site over to Go

--- hawkthorne.go
+++ hawkthorne.go
@@ -3,114 +3,33 @@ package main
 import (
 	"encoding/base64"
 	"encoding/json"
-	"fmt"
 	"html/template"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
 	"os"
-	"regexp"
-	"strings"
 )
 
-type Website struct {
-	Pattern     string
-	UserURL     string
-	Name        string
-	NotFound    string
-	RegisterURL string
-}
-
-type Config struct {
-	Sites []Website
-}
-
-type handleResult struct {
-	Site      Website
-	Available bool
-	Profile string
-}
-
-func readConfig() Config {
-	if len(os.Args) != 2 {
-		log.Fatal("You must supply a configuration filename")
-	}
-	filename := os.Args[1]
-	bytes, err := ioutil.ReadFile(filename)
-	if err != nil {
-		panic(err)
-	}
-	var c Config
-	err = json.Unmarshal(bytes, &c)
-	if err != nil {
-		panic(err)
-	}
-	return c
-}
-
-func checkHandle(handle string, site Website) bool {
-
-	valid, err := regexp.MatchString(site.Pattern, handle)
-
-	if !valid || err != nil {
-		return false
-	}
-
-	res, err := http.Get(fmt.Sprintf(site.UserURL, handle))
-
-	if err != nil {
-		return false
-	}
-
-	defer res.Body.Close()
-	body, err := ioutil.ReadAll(res.Body)
-
-	if err != nil {
-		return false
-	}
-
-	if site.NotFound == "" {
-		return res.StatusCode == 404 || string(body) == ""
-	}
-
-	return strings.Contains(string(body), site.NotFound)
-}
-
-func main() {
-	tmpl, err := template.ParseFiles("index.html")
+func renderTemplate(path string) func(http.ResponseWriter, *http.Request) {
+	tmpl, err := template.ParseFiles("templates/base.html", "templates/"+path)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	config := readConfig()
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
 		tmpl.Execute(w, nil)
-	})
+	}
+}
 
-	http.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request) {
-		f, ok := w.(http.Flusher)
-
-		if !ok {
-			http.Error(w, "streaming unsupported", http.StatusInternalServerError)
-			return
-		}
-
-		w.Header().Set("Content-Type", "text/event-stream")
-		w.Header().Set("Cache-Control", "no-cache")
-		w.Header().Set("Connection", "keep-alive")
-
-		cm := make(chan handleResult)
-
+func trackDownload(path string) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
 		go func() {
 			datum := map[string]interface{}{
-				"event": "search",
+				"event": "game.downloaded",
 				"properties": map[string]interface{}{
-					"token":  os.Getenv("HANDLE_MIXPANEL_TOKEN"),
-					"ip":     r.Header.Get("X-Forwarded-For"),
-					"handle": r.FormValue("handle"),
+					"token": os.Getenv("MIXPANEL_TOKEN"),
+					"ip":    r.Header.Get("X-Forwarded-For"),
 				},
 			}
 
@@ -125,31 +44,26 @@ func main() {
 			_, _ = http.Get("http://api.mixpanel.com/track/?" + v.Encode())
 		}()
 
-		for _, site := range config.Sites {
-			go func(site Website) {
-				a := checkHandle(r.FormValue("handle"), site)
-				profile := fmt.Sprintf(site.UserURL, r.FormValue("handle"))
-				cm <- handleResult{Site: site, Available: a, Profile: profile}
-			}(site)
-		}
+		url := "https://s3.amazonaws.com/hawkthorne.journey.builds/" + path
+		http.Redirect(w, r, url, http.StatusFound)
+	}
+}
 
-		for _, _ = range config.Sites {
-			hr, ok := <-cm
+func main() {
+	http.HandleFunc("/", renderTemplate("index.html"))
+	http.HandleFunc("/sprites", renderTemplate("sprites.html"))
+	http.HandleFunc("/soundtrack/disc/1", renderTemplate("disc1.html"))
+	http.HandleFunc("/soundtrack/disc/2", renderTemplate("disc2.html"))
 
-			if !ok {
-				continue
-			}
+	http.HandleFunc("/downloads/hawkthorne-win-x64.zip", trackDownload("hawkthorne-win-x64.zip"))
+	http.HandleFunc("/downloads/hawkthorne-win-x86.zip", trackDownload("hawkthorne-win-x86.zip"))
+	http.HandleFunc("/downloads/hawkthorne-osx.zip", trackDownload("hawkthrone-osx.zip"))
+	http.HandleFunc("/downloads/hawkthorne.love", trackDownload("hawkthorne.love"))
 
-			b, err := json.Marshal(hr)
-
-			if err != nil {
-				continue
-			}
-
-			fmt.Fprintf(w, "data: %s\n\n", string(b))
-			f.Flush()
-		}
-	})
+	http.Handle("/sprites.html", http.RedirectHandler("/sprites", 301))
+	http.Handle("/audio.html", http.RedirectHandler("/soundtrack/disc/1", 301))
+	http.Handle("/soundtrack.html", http.RedirectHandler("/soundtrack/disc/1", 301))
+	http.Handle("/soundtrack-disc2.html", http.RedirectHandler("/soundtrack/disc/2", 301))
 
 	port := os.Getenv("PORT")
 
